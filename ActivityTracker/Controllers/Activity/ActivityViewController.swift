5a2cc1a5421869b697efa6b0ac29316551a0a9b5
Vijith TV <VijithTV@ndimensionzs-Mac-mini.local>
2019-03-26 18:50:57 +0530

    Get walk data and save

--- ActivityTracker/Controllers/Activity/ActivityViewController.swift
+++ ActivityTracker/Controllers/Activity/ActivityViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ActivityViewController: UIViewController {
+    
+    private var walkingBegan: Bool
+    private var instantPace: Double
+    private var seconds: Double
+    private var distance: Double
+    private var instantPace: Double
+    
+    private let manager = Locations.locationManager
+    private let activity = Activity()
+    
+    lazy var location: [CLLocation] = []
+    lazy var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+    }
 
+    public mutating func startWalking() {
+        
+        if !walkingBegan {
+            walkingBegan = true
+            distance = 0.0
+            seconds = 0
+            location.removeAll(keepingCapacity: false)
+            timer = Timer.scheduledTimer(timeInterval: 1,
+                                         target: self,
+                                         selector: #selector(self.walking),
+                                         userInfo: nil,
+                                         repeats: true)
+        } else {
+            walkingBegan = false
+            stopWalking()
+            
+        }
+    }
+    
+    private mutating func walking() {
+        seconds += 1
+        let distanceCovered = HKQuantity(unit: HKUnit.mile(), doubleValue: distance)
+        let paceUnit = HKUnit.second().unitDivided(by: HKUnit.mile())
+        let liveSpeed = HKQuantity(unit: paceUnit, doubleValue: seconds / distance)
+    }
+    
+    private mutating func stopWalking() {
+        stopTimer()
+    }
+    
+    private mutating func stopTimer()  {
+        self.timer.invalidate()
+    }
+    
+    private func saveWalk() {
+        
+        activity.averageSpeed = self.instantPace
+        activity.distance = self.distance
+        activity.save()
+        
+    }
 
 }
 
--- ActivityTracker/ViewModel/ActivityViewModel/ActivityViewModel.swift
+++ ActivityTracker/ViewModel/ActivityViewModel/ActivityViewModel.swift
@@ -7,18 +7,11 @@
 //
 
 import Foundation
-import CoreLocation
-import HealthKit
 
 public struct ActivityViewModel {
     
     private let activity: Activity
-    private var walkingBegan: Bool
-    private var instantPace: Double
-    private var seconds: Int
     
-    lazy var location: [CLLocation] = []
-    lazy var timer = Timer()
     
     /// Initializer
     ///
@@ -40,33 +33,5 @@ public struct ActivityViewModel {
     /// Steps
     public var steps: Int {
         return activity.steps
-    }
-    
-    public mutating func startWalking() {
-        
-        if !walkingBegan {
-            walkingBegan = true
-            activity.distance = 0.0
-            seconds = 0
-            location.removeAll(keepingCapacity: false)
-        }
-    }
-    
-    private mutating func walking() {
-        seconds += 1
-        
-        let distanceCovered = HKQuantity(unit: HKUnit.mile(), doubleValue: activity.distance)
-        let paceUnit = HKUnit.second().unitDivided(by: HKUnit.mile())
-        let liveSpeed = HKQuantity(unit: paceUnit, doubleValue: Double(seconds) / activity.distance)
-        activity.averageSpeed = liveSpeed
-    }
-    
-    private mutating func stopWalking() {
-        
-    }
-    
-    private mutating func stopTimer()  {
-        
-        self.timer.invalidate()
     }
 }
